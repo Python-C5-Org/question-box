b0d0a935870e06e5ffe3e8633a0e58d42acca9ad
Kathleen Rauh <krauh2010@gmail.com>
2015-10-23 11:23:22 -0400

    added most of heroku material, beginning of models for scores.

--- question_box/questionapp/models.py
+++ question_box/questionapp/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
 
 
 class Question(models.Model):
@@ -8,16 +6,43 @@ class Question(models.Model):
     text = models.TextField()
     profile = models.ForeignKey(User)
     timestamp = models.DateTimeField(blank=True, null=True)
+    score = SmallIntegerField()
 
-    def __str__(self):
-        return self.title
+    def __str__(self, title, score):
+        self.title = title
+        self.score = 0
 
 
 class Answer(models.Model):
     text = models.TextField()
     profile = models.ForeignKey(User)
     question = models.ForeignKey('Question')
-    timestamp = models.DateTimeField(blank=True, null=True)
+    timestamp = models.DateTimeField(null=True)
+    score = SmallIntegerField()
+
+    def __str__(self, text, score):
+        self.text = text
+        self.score = 0
+
+
+class UserScore(models.Model):
+    user_score = SmallIntegerField()
 
     def __str__(self):
-        return self.text
+        return self.id
+
+
+class AnswerScore(models.Model):
+    answer_id = models.ForeignKey(Profile)
+    answer_score = SmallIntegerField()
+
+    def __str__(self):
+        return self.id
+
+
+class QuestionScore(models.Model):
+    question_id = models.ForeignKey(Profile)
+    question_score = SmallIntegerField()
+
+    def __str__(self):
+        return self.id
