d01c0a327e7c06b9c38b629eb29071f95ee16fed
Ryan Burton <rryanburton@gmail.com>
2015-10-23 14:44:25 -0400

    submit question working

--- question_box/questionapp/views.py
+++ question_box/questionapp/views.py
@@ -1,54 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 # from django.views.generic import CreateView
-# from django.contrib.auth.decorators import login_required
-# from datetime import datetime
-# from .forms import QuestionCreateForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from .forms import QuestionCreateForm
 # from .models import Question
 # Create your views here.
-
 
 
 def index(request):
     return render(request, 'questionapp/index.html')
 
-def new_question(request):
-    return render(request, 'questionapp/new_question.html')
 
 def new_answer(request):
     return render(request, 'questionapp/new_answer.html')
 
-# class CreateQuestion(CreateView):
-#     model = Question
-#     fields = ['title', 'text']
-#
-#     @login_required
-#     def get(self, request):
-#         return super().get(request)
-#
-#     @login_required
-#     def post(self, request):
-#         title = request.POST['title']
-#         text = request.POST['text']
-#
-#         Question.objects.create(title=title, text=text,
-#                                 profile=request.user.profile,
-#                                 timestamp=datetime.utcnow())
-#
-#     return render(request, 'questionapp/new_question.html', {'form': form})
-#
 
-# @login_required
-# def add_bookmark(request):
-#     if request.method == 'POST':
-#         form = BookmarkForm(request.POST)
-#         if form.is_valid():
-#             bookmark = form.save(commit=False)
-#             bookmark.user = request.user
-#             bookmark.generate_short()
-#             bookmark.modified = datetime.now()
-#             bookmark.save()
-#             return redirect('recent')
-#     else:
-#         form = BookmarkForm()
-#     return render(request, 'crisco/add_bookmark.html',
-#                   {'form': form})
+@login_required
+def new_question(request):
+    if request.method == 'POST':
+        form = QuestionCreateForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.profile = request.user
+
+            question.timestamp = datetime.now()
+            question.save()
+            return redirect('index')
+    else:
+        form = QuestionCreateForm()
+    return render(request, 'questionapp/new_question.html',
+                  {'form': form})
