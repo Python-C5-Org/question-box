e928ca7d1d5fa2eb476604a913ad69cef22733cb
Ryan Burton <rryanburton@gmail.com>
2015-10-23 11:27:31 -0400

    index page showing

--- question_box/question_box/urls.py
+++ question_box/question_box/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from questionapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index')
     # url(r'^question/new', 'question.views.new_question', name='new_question'),
     # url(r'^answer/new', 'answer.views.new_answer', name='new_answer'),
 
