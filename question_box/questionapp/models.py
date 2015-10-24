e63155211c34950202692d5bb56c8abc98b152d1
Ryan Burton <rryanburton@gmail.com>
2015-10-24 12:20:13 -0400

    added tag to models.py

--- question_box/questionapp/models.py
+++ question_box/questionapp/models.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Tag(models.Model):
+    text = models.CharField(max_length=50)
+
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     profile = models.ForeignKey(User)
     timestamp = models.DateTimeField(blank=True, null=True)
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
